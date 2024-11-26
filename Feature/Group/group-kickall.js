53ef5f8032bfddad170eafeacc4e4d46bae910e6
selxyz <celestialdevcall@gmail.com>
2024-11-26 21:48:45 +0700

    Add files via upload

--- Feature/Group/group-kickall.js
+++ Feature/Group/group-kickall.js
@@ -1,1 +1,21 @@
+let handler = async (m, { conn }) => {
+  let owner = '6282181938329@s.whatsapp.net'; // Nomor owner
+  let participants = await conn.groupMetadata(m.chat).then(group => group.participants);
+  let users = participants
+    .map(participant => participant.id)
+    .filter(user => !(user === owner || user === conn.user.jid));
 
+  for (let user of users) {
+    if (user.endsWith('@s.whatsapp.net')) {
+      await conn.groupParticipantsUpdate(m.chat, [user], "remove");
+    }
+  }
+};
+handler.help = ['kickall'];
+handler.tags = ['admin'];
+handler.command = /^(kickall)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
